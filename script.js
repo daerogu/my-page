1a88392570781b7469dbea454e58b5b403178d8d
daero <rneofh@kaist.ac.kr>
2026-09-20 18:08:32 +0900

    자기소개 및 GOAT모드 추가

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const btn = document.querySelector("#theme-btn");
+const goatBtn = document.getElementById("goat-btn");
 
+// 다크모드
 btn.addEventListener("click", () => {
+  // GOAT모드가 켜져 있으면 끄기
+  document.body.classList.remove("goat");
+
+  // 다크모드 ON / OFF
   document.body.classList.toggle("dark");
-}); 
+});
+
+// GOAT모드
+goatBtn.addEventListener("click", () => {
+  // 다크모드가 켜져 있으면 끄기
+  document.body.classList.remove("dark");
+
+  // GOAT모드 ON / OFF
+  document.body.classList.toggle("goat");
+});
